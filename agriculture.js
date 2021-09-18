df7748437fae09a9d683b701db7b2056c461bf69
IshaanSohi <79596209+IshaanSohi@users.noreply.github.com>
2021-09-18 22:43:55 +0530

    Agri done

---
+++ agriculture.js
@@ -0,0 +1,11 @@
+function smallFarmer() {
+    $(".small").toggleClass("hide");
+}
+
+function otherFarmer() {
+    $(".others").toggleClass("hide");
+}
+
+function allFarmer() {
+    $(".all").toggleClass("hide");
+}
